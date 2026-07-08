9296564c61a744b51cf2a2cacaea8142524395b2
Abood-Najim <abdo0287659@gmail.com>
2026-07-08 13:41:18 -0700

    Deleted some unneeded Sore files and creating a new one

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,15 +1,12 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import authAxiosInstance from '../../api/authAxiosInstance';
-import { useUserStore } from '../../store/useUserStore';
-import { useCounterStore } from '../../store/useCounterStore';
+
 
 
 export default function Cart() {
 
   
-  const counter = useCounterStore((state)=> state.counter);
-  const increment = useCounterStore((state)=> state.increment);
   const getItems = async ()=>{
     const response = await authAxiosInstance.get(`/Carts`);
     console.log(response);
@@ -21,8 +18,7 @@ export default function Cart() {
 
   return (
     <>
-    <div>Cart - {counter} </div>
-    <button onClick={increment}> increment </button>
+    <div>Cart</div>
     
     </>
   )
--- src/store/useUserStore.jsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-
-export const useUserStore = create( ()=>({
-  userName:'Abood'
-}))
-/* i add any var i want it here  */ 
