6047529f04d13fc094ed3ead92182ff6aaf76bd2
Abood-Najim <abdo0287659@gmail.com>
2026-07-08 07:37:51 -0700

    authaxiosinstance&&cart token done

---
+++ src/api/authAxiosInstance.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const token = localStorage.getItem("accessToken"); 
+
+const authAxiosInstance = axios.create(
+  {
+    baseURL:`${import.meta.env.VITE_BURL}`, /*any place i use axoisInstance in it wil see the things that are here so when i want to modify it i just came here (clean and nice) */
+    headers:{
+      "Accept-Language" : "en",
+      "Authorization":`Baerer ${token}`
+    }
+  }
+);
+
+export default authAxiosInstance;
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,15 @@
+import axios from 'axios'
 import React from 'react'
+import authAxiosInstance from '../../api/authAxiosInstance';
 
 export default function Cart() {
+
+  const getItems = async ()=>{
+    const response = await authAxiosInstance.get(`/Carts`);
+    console.log(response);
+  }
+
+
   return (
     <div>Cart</div>
   )
