a662b346ddf7041dc464b0a2465cb93e7242af24
Abood-Najim <abdo0287659@gmail.com>
2026-07-08 13:46:13 -0700

    Deleting lines from navbar that are related to useCounterStore

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useCounterStore } from '../../store/useCounterStore'
 
 export default function Navbar() {
   
-  const counter = useCounterStore((state)=> state.counter)
   return (
     <nav>
-      <Link to="/">Home - {counter}</Link>
+      <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
       <Link to="/cart">Cart</Link>
