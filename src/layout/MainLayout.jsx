039ddc71a312cc41e418a3c5512a6e3dd8f64d9b
Abood-Najim <abdo0287659@gmail.com>
2026-07-21 00:35:00 -0700

    Removed footer from register and login

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 export default function MainLayout() {
+  const location = useLocation();
   return (
     <>
     <Navbar />
     <Outlet />
-    <Footer />
+    {location.pathname !== '/register' && location.pathname !== '/login' && <Footer />}
     </>
   )
 }
