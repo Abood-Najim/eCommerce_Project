1c9691f3f015f9c8c46f9668396e553e644ea423
Abood-Najim <abdo0287659@gmail.com>
2026-08-01 02:45:24 -0700

    Added a validation for the phone number

--- src/validations/RegisterSchema.jsx
+++ src/validations/RegisterSchema.jsx
@@ -3,7 +3,10 @@ import * as yup from "yup"
 export const registerSchema = yup.object({
         UserName: yup.string().required().min(3).max(20),
         FullName: yup.string().required().min(6).max(30),
-        PhoneNumber: yup.string().required(),
+        PhoneNumber: yup.string().required().matches(
+      /^[0-9]$/,
+      "Please enter a valid Phone Number",
+    ),
         Email: yup.string().email().required() .matches(
       /^[A-Za-z0-9._]+@(gmail\.com|yahoo\.com|icloud\.com|outlook\.com|hotmail\.com)$/,
       "Please enter a valid Gmail, Yahoo, iCloud or Microsoft email address.",
