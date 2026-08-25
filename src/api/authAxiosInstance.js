ab0d8293ec92d22dc72eb575183c4511084c9dc5
Abood-Najim <abdo0287659@gmail.com>
2026-08-25 13:10:08 -0700

    Fixed the infinite loop in the fresh users profile

--- src/api/authAxiosInstance.js
+++ src/api/authAxiosInstance.js
@@ -2,17 +2,18 @@ import axios from "axios";
 import useAuthStore from "../store/useAuthStore";
 import i18n from "../i18next";
 
-const token = useAuthStore.getState().token;
+
 const authAxiosInstance = axios.create(
   {
-    baseURL: `${import.meta.env.VITE_BURL}`, /*any place i use axoisInstance in it wil see the things that are here so when i want to modify it i just came here (clean and nice) */
-    headers: {
-      "Authorization": `Bearer ${token}` /* sends the authantication that i'm the user Abood for example */
-    }
+    baseURL:`${import.meta.env.VITE_BURL}`, /*any place i use axoisInstance in it wil see the things that are here so when i want to modify it i just came here (clean and nice) */
   }
 );
 
 authAxiosInstance.interceptors.request.use((config)=>{
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`; /* sends the authantication that i'm the user Abood for example */
+  }
   config.headers["Accept-Language"] = i18n.language;
   return config;
 })
