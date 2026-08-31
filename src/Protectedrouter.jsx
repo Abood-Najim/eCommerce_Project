80abeaa56d1e8216c032254ab535d4dd8063988b
Abood-Najim <abdo0287659@gmail.com>
2026-08-31 13:36:31 -0700

    checkout when cart empty fixed

--- src/Protectedrouter.jsx
+++ src/Protectedrouter.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect } from 'react'
-import useAuthStore from './store/useAuthStore'
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import useAuthStore from './store/useAuthStore';
+import { Navigate, useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { useTranslation } from 'react-i18next'
-export default function Protectedrouter({children}) {
+import { useTranslation } from 'react-i18next';
+import useCart from './hooks/useCart';
 
+export default function Protectedrouter({ children }) {
   const token = useAuthStore((state) => state.token);
   const { t } = useTranslation();
+  const location = useLocation();
+  
+  const { data } = useCart();
+  const items = data?.items || [];
 
   useEffect(() => {
     if (!token) {
       toast.warning(t('Please login to access this page'));
     }
-  }, [token, t]);
-  if(!token){
-    return <Navigate to='/login' />;
+
+    if (token && location.pathname === '/checkout' && items.length === 0) {
+      toast.dismiss();
+      toast.warning(t('Your Cart Is Empty Buy Something'));
+    }
+  }, [token, t, location.pathname, items.length]);
+
+  if (!token) {
+    return <Navigate to='/login' replace />;
+  }
+
+  if (location.pathname === '/checkout' && items.length === 0) {
+    return <Navigate to='/cart'/>;
   }
   
   return children;
